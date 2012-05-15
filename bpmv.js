b96b58cf645606547aeb6b179c8e4b5b6f2edbfc
Bryn Mosher <sites@brynmosher.com>
2012-05-15 15:15:20 -0700

    Added "selected" to positive values for bpmv.trueish() - Thanks Tom!

--- bpmv.js
+++ bpmv.js
@@ -856,7 +856,7 @@
 				case 'function':
 					return String(maybe); // we return a string for safety - no calling the func!
 				case 'string':
-					return (/^\s*(on|true|yes|1|yar|checked)\s*$/i).test(maybe);
+					return (/^\s*(on|true|yes|1|yar|checked|selected)\s*$/i).test(maybe);
 					break;
 				case 'object':
 					return this.obj( maybe, true );
